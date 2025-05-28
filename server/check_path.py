ca399bb6de9c607ff24561a0946943c4cf67eb0e
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2025-05-29 02:39:55 +0300

    Add files via upload

---
+++ server/check_path.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Load the data from the CSV file
+df = pd.read_csv("C:/Users/USER 1/OneDrive/Desktop/issync_departmental_manager/server/Students.csv")
+
+# Print column names to check if the column exists
+print(df.columns)
